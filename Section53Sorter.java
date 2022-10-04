8a3c631ec565b8e9b29272197fe2c556dd46425e
alothmanmj <alothman.maj@gmail.com>
2022-10-04 14:31:36 +0300

    Create method header for sort

--- Section53Sorter.java
+++ Section53Sorter.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class Section53Sorter{
+    static void sort(int[] arr){
+        System.out.println("to be impl");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
-
+        int [] arr = {3,2,5,8,9};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
-}
+} 
